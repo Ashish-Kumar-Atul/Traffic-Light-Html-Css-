9a6f04637efedaaea4f9f3cced4064219a8ff3dc
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-01-24 21:09:52 +0530

    Project Completed

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const redColor = document.querySelector('.red')
 const yellow = document.querySelector('.yellow')
 const green = document.querySelector('.green')
+const love = document.querySelector('.base')
+const main = document.querySelector('main')
 
 redColor.addEventListener('click', () => {
     redColor.style.backgroundColor = 'red';
 });
-yellow.addEventListener('click',function(){
+yellow.addEventListener('click',()=>{
     yellow.style.backgroundColor='yellow'
 });
-green.addEventListener('click',function(){
+green.addEventListener('click',()=>{
     green.style.backgroundColor='green'
+});
+love.addEventListener('dblclick',()=>{
+    love.innerHTML='<h1 class="love">Made with Love ❤️</h1>';
+});
+main.addEventListener('click',()=>{
+    main.style.backgroundColor='white'
 });
