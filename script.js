37dad99a6fbec38a0fed114b166736f7151b4cfc
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-01-25 10:53:05 +0530

    Click ture false added.

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const redColor = document.querySelector('.red')
+const red = document.querySelector('.red')
 const yellow = document.querySelector('.yellow')
 const green = document.querySelector('.green')
 const love = document.querySelector('.base')
 const main = document.querySelector('main')
 
-redColor.addEventListener('click', () => {
-    redColor.style.backgroundColor = 'red';
+let redColorChanger = true;
+let yellowColorChanger = true;
+let greenColorChanger = true;
+
+
+red.addEventListener('click', () => {
+    if (redColorChanger===true) {
+        red.style.backgroundColor = 'red';
+        redChanger=false;
+    } else {
+        red.style.backgroundColor = 'rgb(215, 86, 86)'
+        redColorChanger=true
+    }
 });
-yellow.addEventListener('click',()=>{
-    yellow.style.backgroundColor='yellow'
+
+yellow.addEventListener('click', () => {
+    if (yellowColorChanger===true) {
+        yellow.style.backgroundColor = 'yellow';
+        yellowColorChanger=false;
+    } else {
+        yellow.style.backgroundColor = 'rgb(248, 248, 170)'
+        yellowColorChanger=true
+    }
 });
-green.addEventListener('click',()=>{
-    green.style.backgroundColor='green'
-});
-love.addEventListener('dblclick',()=>{
-    love.innerHTML='<h1 class="love">Made with Love ❤️</h1>';
+
+green.addEventListener('click', () => {
+    if (greenColorChanger===true) {
+        green.style.backgroundColor = 'green';
+        greenColorChanger=false;
+    } else {
+        green.style.backgroundColor = 'rgb(97, 145, 97)'
+        greenColorChanger=true
+    }
 });
 main.addEventListener('click',()=>{
     main.style.backgroundColor='white'
