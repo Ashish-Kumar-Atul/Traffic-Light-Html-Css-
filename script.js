ac2b126b1ea81a32559bb1dc6ea47cf62af38906
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-02-01 19:26:15 +0530

    Added Content Loader

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-const red = document.querySelector('.red')
-const yellow = document.querySelector('.yellow')
-const green = document.querySelector('.green')
-const love = document.querySelector('.base')
-const main = document.querySelector('main')
-
-let redColorChanger = true;
-let yellowColorChanger = true;
-let greenColorChanger = true;
-
-
-red.addEventListener('click', () => {
-    if (redColorChanger===true) {
-        red.style.backgroundColor = 'red';
-        redChanger=false;
-    } else {
-        red.style.backgroundColor = 'rgb(215, 86, 86)'
-        redColorChanger=true
-    }
-});
-
-yellow.addEventListener('click', () => {
-    if (yellowColorChanger===true) {
-        yellow.style.backgroundColor = 'yellow';
-        yellowColorChanger=false;
-    } else {
-        yellow.style.backgroundColor = 'rgb(248, 248, 170)'
-        yellowColorChanger=true
-    }
-});
-
-green.addEventListener('click', () => {
-    if (greenColorChanger===true) {
-        green.style.backgroundColor = 'green';
-        greenColorChanger=false;
-    } else {
-        green.style.backgroundColor = 'rgb(97, 145, 97)'
-        greenColorChanger=true
-    }
-});
-main.addEventListener('click',()=>{
-    main.style.backgroundColor='white'
-});
+document.addEventListener('DOMContentLoaded',()=>{
+    
+    const red = document.querySelector('.red')
+    const yellow = document.querySelector('.yellow')
+    const green = document.querySelector('.green')
+    const love = document.querySelector('.base')
+    const main = document.querySelector('main')
+    
+    let redColorChanger = true;
+    let yellowColorChanger = true;
+    let greenColorChanger = true;
+    
+    
+    red.addEventListener('click', () => {
+        if (redColorChanger===true) {
+            red.style.backgroundColor = 'red';
+            redChanger=false;
+        } else {
+            red.style.backgroundColor = 'rgb(215, 86, 86)'
+            redColorChanger=true
+        }
+    });
+    
+    yellow.addEventListener('click', () => {
+        if (yellowColorChanger===true) {
+            yellow.style.backgroundColor = 'yellow';
+            yellowColorChanger=false;
+        } else {
+            yellow.style.backgroundColor = 'rgb(248, 248, 170)'
+            yellowColorChanger=true
+        }
+    });
+    
+    green.addEventListener('click', () => {
+        if (greenColorChanger===true) {
+            green.style.backgroundColor = 'green';
+            greenColorChanger=false;
+        } else {
+            green.style.backgroundColor = 'rgb(97, 145, 97)'
+            greenColorChanger=true
+        }
+    });
+    main.addEventListener('click',()=>{
+        main.style.backgroundColor='white'
+    });
+    
+})
